0f0c4a13bb9718c12a1857556cdcede4bb4882a1
Jakub Pituła <jakub.pitula@mobilems.pl>
2019-09-04 21:37:07 +0200

    Teachers' choice, trying to pass categories

--- src/Controller/VoteController.php
+++ src/Controller/VoteController.php
@@ -11,33 +11,33 @@ use App\Form\Type\VoteType;
 
 class VoteController extends AbstractController
 {
-    /**
-     * @Route("/", name="vote")
-     */
+    // /**
+    //  * @Route("/", name="vote")
+    //  */
 
-    public function new (Request $request)
-    {
-        $vote = new Vote();
+    // public function new (Request $request)
+    // {
+    //     $vote = new Vote();
 
-        $form = $this->createForm(VoteType::class, $vote);
+    //     $form = $this->createForm(VoteType::class, $vote, array(''));
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $vote = $form->getData();
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         // $form->getData() holds the submitted values
+    //         // but, the original `$task` variable has also been updated
+    //         $vote = $form->getData();
     
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($vote);
-            $entityManager->flush();
+    //         // ... perform some action, such as saving the task to the database
+    //         // for example, if Task is a Doctrine entity, save it!
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($vote);
+    //         $entityManager->flush();
     
-            return $this->redirectToRoute('vote');
-        }
+    //         return $this->redirectToRoute('vote');
+    //     }
 
-        return $this->render('vote/new.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
+    //     return $this->render('vote/new.html.twig', [
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 }
