35f96c64e37a2f5b9f8227512edde75111ce7cd1
Jakub Pituła <jakub.pitula@mobilems.pl>
2019-09-24 13:08:26 +0200

    Authorization, login

---
+++ src/DataFixtures/TeacherFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Teacher;
+
+class TeacherFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+        for($i=0; $i<50; $i++){
+            $teacher = new Teacher();
+            $teacher->setName('teacher'.$i);
+            $manager->persist($teacher);
+        }
+
+        $manager->flush();
+    }
+}
